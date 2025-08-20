f8bf1dc440ebb6cf47d32e82b81f0cf97a85f3b1
guironnet <guironne@yahoo.fr>
2025-08-20 04:18:03 +0200

    fix arrange

--- test/simplifyExpression/basicsSearch/rearrangeCoefficient.test.js
+++ test/simplifyExpression/basicsSearch/rearrangeCoefficient.test.js
@@ -7,6 +7,8 @@ describe('rearrangeCoefficient', function() {
     ['2 * x^2', '2 x ^ 2'],
     ['y^3 * 5', '5 y ^ 3'],
     ['x * 5', '5 x'],
+    ['x * y + z', 'x * y + z'],
+    ['x * y', 'x y'],
     ['x * (-5)', '-5 x'],
   ];
   tests.forEach(t => testSimplify(t[0], t[1], rearrangeCoefficient));
