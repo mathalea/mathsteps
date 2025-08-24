f5eef8e50f74ebd0b7160a95a4da8b65fc65c8a1
guironnet <guironne@yahoo.fr>
2025-08-24 12:59:41 +0200

    ajoute du produit en croix

--- test/checks/checks.test.js
+++ test/checks/checks.test.js
@@ -1,9 +1,41 @@
 const checks = require('../../lib/checks');
+const Equation = require('../../lib/equation/Equation');
+const assert = require('assert');
+ const solveEquation = require('../../lib/solveEquation');
 
 const TestUtil = require('../TestUtil');
+const ChangeTypes = require('../../lib/ChangeTypes');
 
 function testCanCombine(exprStr, canCombine) {
   TestUtil.testBooleanFunction(checks.canSimplifyPolynomialTerms, exprStr, canCombine);
+}
+
+function testCanCrossMultiplication(exprStr, canCombine) {
+  const equation = Equation.createEquationFromString(exprStr, '=');
+  const cross = checks.canCrossMultiplication(equation);
+  assert.equal(cross, canCombine);
+}
+
+function testSolveCrossMultiplication(exprStr, outputStr, debug=false, changeType) {
+  const steps = solveEquation(exprStr, debug);
+  const lastStep = steps[steps.length -1].newEquation.ascii();
+  it(exprStr + ' -> ' + outputStr, (done) => {
+      assert.equal(lastStep, outputStr);
+      done();
+  });
+
+  if (changeType){
+    let changeFound = ''
+    steps.forEach(step => {
+      if (changeType === step.changeType) {
+        changeFound = step.changeType;
+      }
+    });
+    it(changeType + ' -> ' + changeFound, (done) => {
+      assert.equal(changeFound, changeType);
+      done();
+    });
+  }
 }
 
 describe('canSimplifyPolynomialTerms multiplication', function() {
@@ -28,4 +60,23 @@ describe('canSimplifyPolynomialTerms addition', function() {
     ['y',  false],
   ];
   tests.forEach(t => testCanCombine(t[0], t[1]));
+});
+
+describe('canCrossMultiplication detection', function() {
+  const tests = [
+    ['2/x = 4/9',  true],
+    ['x/2 = 4/9',  false],
+    ['2/(7x) = 4/9',  true],
+    ['2/7 = 4/x',  true],
+    ['2/2 = x/4',  false],
+    ['4/9 = 2/(7x)',  true],
+  ];
+  tests.forEach(t => testCanCrossMultiplication(t[0], t[1]));
+});
+
+describe('canCrossMultiplication detection', function() {
+  const tests = [
+    ['2/x = 4/9',  'x = 9 / 2', ChangeTypes.CROSS_PRODUCT_EQUALITY],
+  ];
+  tests.forEach(t => testSolveCrossMultiplication(t[0], t[1], false, t[2]));
 });
