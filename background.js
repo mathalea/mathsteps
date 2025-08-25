df29851fd6c2a3577af6ef904f23f09130d30c0e
guironnet <guironne@yahoo.fr>
2025-08-25 12:01:48 +0200

    fix parenthesis if not neccessary for equation

--- background.js
+++ background.js
@@ -46,4 +46,23 @@ steps.forEach(step => {
     console.log("# of substeps: " + step.substeps.length);      // e.g. # of substeps: 2
 })
 
+console.log('=====================')
+steps = mathsteps.solveEquation('5/x + 4 = 7/3');
+
+steps.forEach(step => {
+    console.log("before change: " + step.oldEquation.ascii());  // e.g. before change: 2x + 3x = 35
+    console.log("change: " + step.changeType);                  // e.g. change: SIMPLIFY_LEFT_SIDE
+    console.log("after change: " + step.newEquation.ascii());   // e.g. after change: 5x = 35
+    console.log("# of substeps: " + step.substeps.length);      // e.g. # of substeps: 2
+})
+
+steps.forEach(step => {
+    console.log("before change: " + step.oldEquation.latex());  // e.g. before change: 2x + 3x = 35
+    console.log("change: " + step.changeType);                  // e.g. change: SIMPLIFY_LEFT_SIDE
+    console.log("after change: " + step.newEquation.latex());   // e.g. after change: 5x = 35
+    console.log("# of substeps: " + step.substeps.length);      // e.g. # of substeps: 2
+})
+
+
+
 const changes = mathsteps.ChangeTypes
